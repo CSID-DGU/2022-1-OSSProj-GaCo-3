f2ce2e3e08015123efc5666f93a57cbe2967b258
alixcoxon <seoinseoulcity@gmail.com>
2022-05-07 02:57:46 +0900

    fix : change outside map

--- code/level.py
+++ code/level.py
@@ -10,8 +10,6 @@ from debug import *
 
 class Level:
     def __init__(self):
-        self.player_size = (200, 200)
-
         #get the display surface
         self.display_surface = pygame.display.get_surface()
 
@@ -24,7 +22,13 @@ class Level:
 
     def create_map(self):
         # player 생성
+<<<<<<< Updated upstream
         self.player = Player((100, 400), PLAYER_SIZE, [self.visible_sprites], self.obstacle_sprites)
+=======
+        self.player = Player(PLAYER_COOR_ini, PLAYER_SIZE, [self.visible_sprites], self.obstacle_sprites)
+        # monster 생성
+        self.monster = Bringer(BRINGER_COOR_ini, BRINGER_SIZE, [self.visible_sprites], self.obstacle_sprites)
+>>>>>>> Stashed changes
 
     def run(self,df):
         self.visible_sprites.custom_draw(self.player)
@@ -47,17 +51,18 @@ class CameraGroup(pygame.sprite.Group): # Not implemented yet
         self.offset = [0, 0]
 
         # 오프셋 변경 기준 x점
-        self.left_x_point = 639 # 이 오프셋 포인트 기준으로 플레이어가 오른쪽으로 향하면 그 후로는 플레이어가 화면 중심에 있음
-        self.right_x_point = 2800 # 이 오프셋 포인트 기준으로 플레이어가 왼쪽으로 향하면 그 후로는 플레이어가 화면 중심에 있음
+        self.left_x_point = 640 # 이 오프셋 포인트 기준으로 플레이어가 오른쪽으로 향하면 그 후로는 플레이어가 화면 중심에 있음
+        self.right_x_point = 1850 # 이 오프셋 포인트 기준으로 플레이어가 왼쪽으로 향하면 그 후로는 플레이어가 화면 중심에 있음
         # 위의 포인트 사이에 있을 경우 플레이어는 항상 화면 가운데에 있지만
         # left 포인트보다 왼쪽에 있을 경우에는 오프셋은 left_x_point가 기준이된다. 반대의 경우도 마찬가지다. # custom_draw의 해당 코드에 또 주석 달아놓았음
 
         #creating the floor s
-        self.background_sky_surf = pygame.image.load('image/map/Start_Sky.png')
-        self.background_sky_rect = self.background_sky_surf.get_rect(topleft=(0,0))
+        self.background_mountain_surf = pygame.image.load('image/map/forest.png')
+        self.background_mountain_surf = pygame.transform.scale(self.background_mountain_surf, (1900, 570))
+        self.background_mountain_rect = self.background_mountain_surf.get_rect(topleft=(0,0))
 
-        self.background_floor_surf = pygame.image.load('image/map/Start_Map.png')
-        self.background_floor_surf = pygame.transform.scale(self.background_floor_surf, (3500, 897)) # 화면 변화 속도가 달라서 바닥만 대충 크게 리사이즈함. 이미지 리소스 바꾸면 값 바꿀 것
+        self.background_floor_surf = pygame.image.load('image/map/floor_size.png')
+        self.background_floor_surf = pygame.transform.scale(self.background_floor_surf, (2500, 800)) # 화면 변화 속도가 달라서 바닥만 대충 크게 리사이즈함. 이미지 리소스 바꾸면 값 바꿀 것
         self.background_floor_rect = self.background_floor_surf.get_rect(topleft=(0,0))
 
     def custom_draw(self, player):
@@ -72,11 +77,11 @@ class CameraGroup(pygame.sprite.Group): # Not implemented yet
         # 멀리 있는 물체가 더 느리게 변화하고 바닥 물체가 더 빠르게 변화하므로 바닥이 멀리 있는 물체보다 이미지 사이즈가 커야함.
         # 스케일을 맞춰볼까? :
         floor_offset_pos = sub_Coordinate(self.background_floor_rect.topleft, self.offset)
-        sky_offset_pos = sub_Coordinate(self.background_sky_rect.topleft, self.offset)
+        sky_offset_pos = sub_Coordinate(self.background_mountain_rect.topleft, self.offset)
         sky_offset_pos[0] *= 0.3 # 멀리있는 하늘은 x포지션의 변화 속도를 가까이있는 바닥보다 느리게 만들기
 
-        self.display_surface.blit(self.background_sky_surf, sky_offset_pos)
-        self.display_surface.blit(self.background_floor_surf, add_Coordinate(floor_offset_pos, (0, -150)))
+        self.display_surface.blit(self.background_mountain_surf, sky_offset_pos)
+        self.display_surface.blit(self.background_floor_surf, add_Coordinate(floor_offset_pos, (0, -80)))
 
         # 플레이어 히트박스 그리기
         pygame.draw.rect(self.display_surface, (255, 255, 255),
--- code/settings.py
+++ code/settings.py
@@ -6,6 +6,7 @@ FPS     = 60
 
 # player settings
 PLAYER_SIZE = (300, 300)
+PLAYER_COOR_ini = (100, 420) # 플레이어 초기 위치
 PLAYER_IMG_INFO = {'idle': {'idx': 11, 'size': (300, 300)}, 'idleL': {'idx': 11, 'size': (300, 300)},
                      'run': {'idx': 8, 'size': (300, 300)}, 'runL': {'idx': 8, 'size': (300, 300)},
                      'jump': {'idx': 3, 'size': (300, 300)}, 'jumpL': {'idx': 3, 'size': (300, 300)},
@@ -14,4 +15,17 @@ PLAYER_IMG_INFO = {'idle': {'idx': 11, 'size': (300, 300)}, 'idleL': {'idx': 11,
                      'hitted': {'idx': 4, 'size': (300, 300)}, 'hittedL': {'idx': 4, 'size': (300, 300)},
                      'attack1': {'idx': 7, 'size': (300, 300)}, 'attack1L': {'idx': 7, 'size': (300, 300)},
                      'attack2': {'idx': 7, 'size': (300, 300)}, 'attack2L': {'idx': 7, 'size': (300, 300)}
+<<<<<<< Updated upstream
                      }
+=======
+                     }
+# monster2 settings
+BRINGER_SIZE = (420, 279)
+BRINGER_COOR_ini = (700, 335) # 몬스터2(BRINGER) 초기 위치
+BRINGER_IMG_INFO = {'idleL': {'idx': 8, 'size': BRINGER_SIZE}, 'idleR': {'idx': 8, 'size': BRINGER_SIZE},
+                     'walkL': {'idx': 8, 'size': BRINGER_SIZE}, 'walkR': {'idx': 8, 'size': BRINGER_SIZE},
+                     'attackL': {'idx': 10, 'size': BRINGER_SIZE}, 'attackR': {'idx': 10, 'size': BRINGER_SIZE},
+                     'deathL':{'idx': 10, 'size': BRINGER_SIZE}, 'deathR':{'idx': 10, 'size': BRINGER_SIZE}, 
+                     'castL': {'idx': 9, 'size': BRINGER_SIZE}, 'castR': {'idx': 9, 'size': BRINGER_SIZE},
+                     'hurtL': {'idx': 3, 'size': BRINGER_SIZE}, 'hurtR': {'idx': 3, 'size': BRINGER_SIZE}}
+>>>>>>> Stashed changes
