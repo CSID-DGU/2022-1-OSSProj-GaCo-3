8d256583953a8345e70cce5b8f629b77caca6b64
alixcoxon <seoinseoulcity@gmail.com>
2022-05-19 17:24:50 +0900

    feat : add abyss sprite images

---
+++ code/abyss.py
@@ -0,0 +1,12 @@
+from random import getstate
+import pygame
+from settings import *
+from support import *
+from game import *
+from Monster import *
+from level import *
+from debug import *
+
+class Abyss(Monster):
+    def __init__(self, pos):
+        pass
