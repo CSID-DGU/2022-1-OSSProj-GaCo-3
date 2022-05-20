4d592fab489f7627393a730ad8cc92b14157a3bd
alixcoxon <seoinseoulcity@gmail.com>
2022-05-20 14:14:03 +0900

    feat : abyss create on map1

--- code/settings.py
+++ code/settings.py
@@ -4,6 +4,11 @@ WIDTH   = 1280 #가로 크기
 HEIGHT  = 720 #세로 크기
 FPS     = 60
 
+# characters hp info
+PLAYER_HP = 100
+ABYSS_HP = 100
+BRINGER_HP = 150
+
 # player settings
 PLAYER_SIZE = (300, 300)
 PLAYER_COOR_ini = (100, 420) # 플레이어 초기 위치
@@ -16,7 +21,15 @@ PLAYER_IMG_INFO = {'idle': {'idx': 11, 'size': (300, 300)}, 'idleL': {'idx': 11,
                      'attack1': {'idx': 7, 'size': (300, 300)}, 'attack1L': {'idx': 7, 'size': (300, 300)},
                      'attack2': {'idx': 7, 'size': (300, 300)}, 'attack2L': {'idx': 7, 'size': (300, 300)}
                      }
-PLAYER_HP = 100
+
+# monster1 settings - abyss
+ABYSS_SIZE = (300, 300)
+ABYSS_COOR_ini = (700, 385)
+ABYSS_IMG_INFO = {'idleL':{'idx':6, 'size': ABYSS_SIZE}, 'idleR':{'idx':6, 'size': ABYSS_SIZE},
+                  'runL': {'idx': 8, 'size': ABYSS_SIZE}, 'runR': {'idx': 8, 'size': ABYSS_SIZE},
+                  'attack1L': {'idx': 8, 'size': ABYSS_SIZE}, 'attack1R': {'idx': 8, 'size': ABYSS_SIZE},
+                  'deathL':{'idx': 7, 'size': ABYSS_SIZE}, 'deathR':{'idx': 7, 'size': ABYSS_SIZE},
+                  'hurtL': {'idx': 4, 'size': ABYSS_SIZE}, 'hurtR': {'idx': 4, 'size': ABYSS_SIZE} }
 
 # monster2 settings
 BRINGER_SIZE = (420, 279)
@@ -27,7 +40,6 @@ BRINGER_IMG_INFO = {'idleL': {'idx': 8, 'size': BRINGER_SIZE}, 'idleR': {'idx': 
                      'deathL':{'idx': 10, 'size': BRINGER_SIZE}, 'deathR':{'idx': 10, 'size': BRINGER_SIZE}, 
                      'castL': {'idx': 9, 'size': BRINGER_SIZE}, 'castR': {'idx': 9, 'size': BRINGER_SIZE},
                      'hurtL': {'idx': 3, 'size': BRINGER_SIZE}, 'hurtR': {'idx': 3, 'size': BRINGER_SIZE}}
-BRINGER_HP = 100
 
 # 게임 장면 이름
 GAME_STATES = ['intro', 'level1', 'level2', 'level3', 'ending']
